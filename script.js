3c6b4bc3e87ecf5dddae980d1d45874a03dfaf53
jabborov077 <djabborovbehruz2@gmail.com>
2024-05-10 09:59:14 +0500

    joireiog

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// var numVar = 1;
+
+// let numLet = 2;
+
+// const numConst = 3;
+
+// let yigndi = numVar + numLet + numConst;
+
+// console.log(yigndi);
+
+// let str = "Boeing";
+// let num = "747";
+
+// console.log(str + " " + num);
+
+// let firstInteger = +prompt("Birinchi butun sonni kiriting");
+
+// let secondInteger = +prompt("Ikkinchi butun sonni kiriting");
+
+// let bolsin = firstInteger * secondInteger;
+
+// alert("" + firstInteger + " va " + secondInteger + " hosilasi: " + mahsulot);
+
+// function toSquare (){
+//     const num = +prompt("butun son kiriting")
+
+//     return num * num
+// }
+// let natija = toSquare()
+
+// console.log(natija);
